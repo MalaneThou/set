6a87ab33bd3ba80dadb78d0c5d188f26da792a4e
setsar <setsar@gmail.com>
2016-06-22 18:09:19 -0400

    Adding js folder

---
+++ javascript/app.js
@@ -0,0 +1,63 @@
+// variables
+
+/*var aNumber = 100;
+var aString = "This is a string";
+var aBool = true;
+var anArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var anArray2 = ["first string", "second string", "third string"];
+
+
+// all object ie Wisedth, malane should have the same default constructors/values. Object "Malane" only has lastName. This is bad practice.
+var Wisedth = {
+	firstName: "Wisedth",
+	lastName: "Sar",
+	age: 30
+}
+var Malane = {
+	lastName: "Thou"
+}
+var DOB = {
+	Month: ["Jan", "Fed", "Mar", "Apr", "May", "Jun"],
+	Day: [1, 2, 3],
+	Time: {
+		hour: [],
+		min: [],
+		sec: []
+	}
+}
+
+// going over functions
+function alrt() {
+	alert("testing");
+}
+alrt();
+
+
+function addition(num1, num2) {
+	var total = 0;
+	total = Number(num1) + Number(num2);
+	return total; 
+}
+var firstNumber = prompt("Enter your first number: ");
+var secondNumber = prompt("Enter your second number: ");
+
+
+var adding = addition(firstNumber, secondNumber);
+alert("Your total is:" + adding);
+
+
+document.getElementById(); document.querySelector();
+
+
+document.getElementsByClass();
+document.getElementsByTag();
+
+document.querySelectorAll();
+Wisedth.firstName;
+anArray[0];
+*/
+var anArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (var i = 0; i < anArray.length; i++) {
+	console.log(anArray[i]);
+}
